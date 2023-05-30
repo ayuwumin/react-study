bb73209faa32b395d99b2d03fbf387d927a91bef
Aether <tifsnunes@gmail.com>
2023-05-30 18:17:05 -0300

    changing style file - tailwind

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -2,63 +2,63 @@ import React, { useState } from 'react';
 
 
 const App = () => {
-    const[n, setN] = useState(0)
-    const[name, setName] = useState('')
+    // const[n, setN] = useState(0)
+    // const[name, setName] = useState('')
 
-    //*FORMULÁRIO
+
     const [name1, setName1] =useState('')
     const [surname, setSurname] =useState('')
     const [age, setAge] =useState('')
 
-    //* CRIANDO CONTATOR
-    const handleMinus = () => {
-        setN(n-1)
-    }
-    const handleMore = () => {
-        setN(n+1)
-    }
-    //*INPUT COM STATE
-    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setName( event.target.value )
-    }
+
+    // const handleMinus = () => {
+    //     setN(n-1)
+    // }
+    // const handleMore = () => {
+    //     setN(n+1)
+    // }
+
+    // const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setName( event.target.value )
+    // }
 
     return (
-        //* CRIANDO CONTATOR
-        <div>
-            <div>
+
+        <div className="flex justify-center space-y-14">
+            {/* <div>
             <button onClick={handleMinus}>-</button>
             <div>{n}</div>
             <button onClick={handleMore}>+</button>
             </div>
-
-
-            //*INPUT COM STATE
             <div> 
-            Name:
+            
+            
             <input type='text'value={name} onChange={handleInput}/>
             <hr/>
             Your name is: {name}
-            </div>
+            </div> */}
 
-            //* FORMULÁRIO
             <div>
                 <div>
-                    Nome:
-                    <input type='text' value={name1} onChange={(event) => setName1( event.target.value )}/>
+                <h2 className="text-base font-semibold leading-7 text-gray-900">Form</h2>
+                <label className="block text-sm font-medium leading-6 text-gray-900">Name:</label>
+                    <input type='text' value={name1} onChange={(event) => setName1( event.target.value )} className='text-gray-900'/>
                 </div>
 
                 <div>
-                    Sobrenome:
-                    <input type='text' value={surname} onChange={(event) => setSurname( event.target.value )}/>
+                <label className="block text-sm font-medium leading-6 text-gray-900">Sobrenome:</label>
+                    <input type='text' value={surname} onChange={(event) => setSurname( event.target.value )} className='text-gray-900'/>
                 </div>
 
                 <div>
-                    Idade:
-                    <input type='text' value={age} onChange={(event) => setAge( event.target.value )}/>
+                <label className="block text-sm font-medium leading-6 text-gray-900">Age:</label>
+                    <input type='text' value={age} onChange={(event) => setAge( event.target.value )} className='text-gray-900'/>
                 </div>
                 <hr/>
-                Olá {name1} {surname} tudo bem?
-                Você tem {age} anos
+
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Olá {name1} {surname}, sua idade é {age} anos.
+                </label>
             </div>
         </div>
         
