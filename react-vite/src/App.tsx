0d268aee2fe2cca3ddf265384f743ba49654c308
Aether <tifsnunes@gmail.com>
2023-05-30 15:43:20 -0300

    input with state

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 
 const App = () => {
     //* CRIANDO CONTATOR
     const[n, setN] = useState(0)
+    const[name, setName] = useState('')
+
     const handleMinus = () => {
         setN(n-1)
     }
@@ -12,11 +14,21 @@ const App = () => {
         setN(n+1)
     }
 
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName( event.target.value )
+    }
+
     return (
         <div>
             <button onClick={handleMinus}>-</button>
             <div>{n}</div>
             <button onClick={handleMore}>+</button>
+
+            //*INPUT COM STATE
+            Name:
+            <input type='text'value={name} onChange={handleInput}/>
+            <hr/>
+            Your name is: {name}
         </div>
     )    
 }
