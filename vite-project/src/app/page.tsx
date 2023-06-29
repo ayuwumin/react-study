df73fddcfbdcd1f8b22e4578ad34f5fcd7994738
Aether <tifsnunes@gmail.com>
2023-06-29 12:49:00 -0300

    use state

---
+++ vite-project/src/app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useState } from "react";
+
+const Page = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const handleClickButton = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center itens-center text-3xl">
+      <p>{count}</p>
+      <button onClick={handleClickButton} className="bg-blue-500 p-3">
+        +1
+      </button>
+    </div>
+  );
+};
+
+export default Page;
