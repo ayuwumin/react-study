a9759f0a5d03e5abd0782a18ac02e1a5b95d8486
Aether <tifsnunes@gmail.com>
2023-07-05 14:49:52 -0300

    useEffect exemple

--- vite-project/src/app/Effects.tsx
+++ vite-project/src/app/Effects.tsx
@@ -1,28 +1,28 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { VideoPlayer } from "../components/VideoPlayer";
 
 const Page = () => {
-  const [firstName, setFirstName] = useState("Kyoko");
-  const [lastName, setLastName] = useState("Hori");
-
-  const fullName = `${firstName} ${lastName}`;
+  const [playing, setPlaying] = useState(false);
 
   return (
     <div className="">
-      <p>my name is {fullName}</p>
-      <hr />
-      <button
-        className="border border-blue-400 m-3 p-3"
-        onClick={() => setFirstName("Izumi")}
-      >
-        change to Izumi
-      </button>
-      <button
-        className="border border-blue-400 m-3 p-3"
-        onClick={() => setLastName("Miyamura")}
-      >
-        change to Miyamura
-      </button>
+      <div className="border border-white p-3 mb-4">
+        <p className="text-2xl text-blue-400 mb-3">
+          {playing ? "RODANDO" : "PAUSADO"}
+        </p>
+        <button
+          className="bg-blue-400 rounded-md p-3"
+          onClick={() => setPlaying(!playing)}
+        >
+          Play/Pause
+        </button>
+
+        <VideoPlayer
+          src="https://www.youtube.com/watch?v=p5fnHWznTBQ&ab_channel=I-WatchEverythingOnRepeat"
+          isPlaying={playing}
+        />
+      </div>
     </div>
   );
 };
